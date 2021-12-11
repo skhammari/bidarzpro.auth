c4e23ffd39f8d104e0b556fd982e86118fa12d7e
skhammari <skhammari@gmail.com>
2021-12-11 16:41:36 +0330

    Added health route for k8s

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -1,27 +1,27 @@
 <?php
 
-    namespace App\Controller;
+namespace App\Controller;
 
-    use App\Service\KeyManagement;
-    use Exception;
-    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-    use Symfony\Component\HttpFoundation\Response;
-    use Symfony\Component\Routing\Annotation\Route;
+use App\Service\KeyManagement;
+use Exception;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-    class MainController extends AbstractController
+class MainController extends AbstractController
+{
+    #[Route('/getPublicKey', name: 'getPublicKey')]
+    public function getPublicKey(KeyManagement $keyManagement): Response
     {
-        #[Route('/getPublicKey', name: 'getPublicKey')]
-        public function getPublicKey(KeyManagement $keyManagement): Response
-        {
-            $res = new Response($keyManagement->loadPublicKey());
-            $res->headers->set('Content-Type', 'text/plain');
-            return $res;
-        }
+        $res = new Response($keyManagement->loadPublicKey());
+        $res->headers->set('Content-Type', 'text/plain');
+        return $res;
+    }
 
-        /**
-         * @throws Exception
-         */
-        /*#[Route('/verify/{key?}', name: 'verifyKey')]
+    /**
+     * @throws Exception
+     */
+    /*#[Route('/verify/{key?}', name: 'verifyKey')]
         public function verifyKey(KeyManagement $keyManagement, $key): Response
         {
             if (!$key) {
@@ -31,18 +31,24 @@
             return $this->json($keyManagement->decode($key));
         }*/
 
-        /**
-         * @throws Exception
-         */
-        #[Route('/issue/{payload?}', name: 'issueKey')]
-        public function issueKey(KeyManagement $keyManagement, $payload): Response
-        {
-            if (!$payload) {
-                throw new Exception("The json payload is not provided!");
-            }
-
-            return $this->json([
-                'token' => $keyManagement->encode(json_decode($payload))
-            ]);
+    /**
+     * @throws Exception
+     */
+    #[Route('/issue/{payload?}', name: 'issueKey')]
+    public function issueKey(KeyManagement $keyManagement, $payload): Response
+    {
+        if (!$payload) {
+            throw new Exception("The json payload is not provided!");
         }
+
+        return $this->json([
+            'token' => $keyManagement->encode(json_decode($payload))
+        ]);
     }
+
+    #[Route('/health', name: 'health')]
+    public function health()
+    {
+        return new Response('', 200);
+    }
+}
